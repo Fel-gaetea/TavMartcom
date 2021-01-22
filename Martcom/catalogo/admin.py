c4ac0260026a9670fbf6fe42d251c3df431170a5
Fel-gaetea <fel.gaetea@alumnos.duoc.cl>
2021-01-22 02:29:19 -0300

    repositorio,carpeta django,clases BD

---
+++ Martcom/catalogo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from catalogo.models import partner,equipamiento,Cliente,Servicios
+# Register your models here.
+
+admin.site.register(partner)
+admin.site.register(equipamiento)
+admin.site.register(Cliente)
+admin.site.register(Servicios)
+
+# Register your models here.
