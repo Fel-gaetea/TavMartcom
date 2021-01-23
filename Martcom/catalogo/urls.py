ceb687666131f62b7d160220a0794b2bb822d0a1
Fel-gaetea <fel.gaetea@alumnos.duoc.cl>
2021-01-23 03:13:24 -0300

    crud realizado y test

--- Martcom/catalogo/urls.py
+++ Martcom/catalogo/urls.py
@@ -8,3 +8,15 @@ urlpatterns = [
     path('', views.servicios, name='servicios'),
     path('', views.contacto, name='contacto'),
 ]
+
+urlpatterns += [
+    path('Cliente/create/', views.AuthorCreate.as_view(), name='Cliente_create'),
+    path('Cliente/<int:pk>/update/', views.AuthorUpdate.as_view(), name='Cliente_update'),
+    path('Cliente/<int:pk>/delete/', views.AuthorDelete.as_view(), name='Cliente_delete'),
+]
+
+urlpatterns += [
+    path('equipamiento/create/', views.AuthorCreate.as_view(), name='equipamiento_create'),
+    path('equipamiento/<int:pk>/update/', views.AuthorUpdate.as_view(), name='equipamiento_update'),
+    path('equipamiento/<int:pk>/delete/', views.AuthorDelete.as_view(), name='equipamiento_delete'),
+]
--- Martcom/catalogo/views.py
+++ Martcom/catalogo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from . models import Cliente, partner, equipamiento, Servicios
+from . models import Cliente, partner, Servicios, equipamiento
+from django.views.generic.edit import CreateView, UpdateView, DeleteView #CRUD de django
+from django.urls import reverse_lazy #nos permite redireccionar la informacion de la clase 
+from django.views import generic
 
 
 # Create your views here.
@@ -16,15 +19,15 @@ def index(request):
 
         'index.html',
 
-         context={ 'num_equipamiento': num_equipamiento},
+        context={ 'num_equipamiento': num_equipamiento},
     )
 
-    def nosotros(request):
+def nosotros(request):
     
-    return render(
-        request,
-        'nosotros.html',
-    )
+return render(
+    request,
+    'nosotros.html',
+)
 
     def servicios(request):
 
@@ -51,3 +54,64 @@ def index(request):
 
         context={ 'num_clientes': num_clientes},
     )
+
+
+#CRUD cliente
+class ClienteCreate(CreateView):
+    model=Cliente
+    fields='__all__'
+
+class ClienteUpdate(UpdateView):
+    model=Cliente
+    fields=['rut','nom','apell','fono','direc','email']
+
+class ClienteDelete(DeleteView):
+    model=Cliente
+    success_url=reverse_lazy('Cliente')
+
+class ClienteDetailView(generic.DetailView):
+
+    """Generic class-based detail view for an author."""
+
+    model = Cliente
+
+
+#CRUD equipamiento
+class equipamientoCreate(CreateView):
+    model=equipamiento
+    fields='__all__'
+
+class equipamientoUpdate(UpdateView):
+    model=equipamiento
+    fields=['id','name']
+
+class equipamientoDelete(DeleteView):
+    model=equipamiento
+    success_url=reverse_lazy('equipamiento')
+
+#CRUD partner
+class partnerCreate(CreateView):
+    model=partner
+    fields='__all__'
+
+class partnerUpdate(UpdateView):
+    model=partner
+    fields=['name','img']
+
+class partnerDelete(DeleteView):
+    model=partner
+    success_url=reverse_lazy('partner')
+
+#CRUD servcios
+
+class ServiciosCreate(CreateView):
+    model=Servicios
+    fields='__all__'
+
+class ServiciosUpdate(UpdateView):
+    model=Servicios
+    fields=['title','descrip','imag']
+
+class ServiciosDelete(DeleteView):
+    model=Servicios
+    success_url=reverse_lazy('Servicios')
