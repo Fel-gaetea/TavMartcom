09b76109ed2cd7b8d8b49ad49e03378842061acf
Fel-gaetea <fel.gaetea@alumnos.duoc.cl>
2021-01-23 00:02:06 -0300

    templates

--- Martcom/catalogo/urls.py
+++ Martcom/catalogo/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.nosotros, name='nosotros'),
+    path('', views.servicios, name='servicios'),
+    path('', views.contacto, name='contacto'),
 ]
--- Martcom/catalogo/views.py
+++ Martcom/catalogo/views.py
@@ -7,14 +7,8 @@ from . models import Cliente, partner, equipamiento, Servicios
 
 
 def index(request):
-
-    num_clientes = Cliente.objects.all().count()
-
-    num_partner = partner.objects.all().count()
     
     num_equipamiento = equipamiento.objects.all().count()
-
-    num_servicio=Servicios.objects.all().count()
 
     return render(
 
@@ -22,8 +16,38 @@ def index(request):
 
         'index.html',
 
-        context={'num_clientes': num_clientes, 'num_partner': num_partner, 
+         context={ 'num_equipamiento': num_equipamiento},
+    )
 
-        'num_equipamiento': num_equipamiento, 'num_servicios': num_servicio},
+    def nosotros(request):
+    
+    return render(
+        request,
+        'nosotros.html',
+    )
 
+    def servicios(request):
+
+    num_servicio=Servicios.objects.all().count()
+    
+    return render(
+
+        request,
+
+        'servicios.html',
+
+        context={ 'num_servicios': num_serviciosr},
+    )
+
+    def contacto(request):
+
+    num_clientes = Cliente.objects.all().count()
+    
+    return render(
+
+        request,
+
+        'contacto.html',
+
+        context={ 'num_clientes': num_clientes},
     )
