c8678654313a898c5fe94c38352cd774175c7816
Fel-gaetea <fel.gaetea@alumnos.duoc.cl>
2021-01-27 21:25:02 -0300

    cerrar sesion

--- Martcom/Martcom/settings.py
+++ Martcom/Martcom/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
